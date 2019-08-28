51954241fad2bc47e966044512c27aeea772207b
Sarthak Sharma <7lovesharma7@gmail.com>
2019-08-28 15:44:28 +0530

    basic pages added

--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
-let start = document.getElementById("start");
-let addGif = document.getElementById("exercise-gif");
-let alert = document.getElementById("alert");
+// let start = document.getElementById("start");
+// let addGif = document.getElementById("exercise-gif");
+// let alert = document.getElementById("alert");
+let view = document.getElementById("view");
+let timeClock = document.getElementById("timeClock");
+let exercise = document.getElementById("exercise");
+let search = document.getElementById("search");
 
-let audio = new Audio("../assets/alert.mp3");
+let timer = new easytimer.Timer();
 
-let exercise = ["burpees", "crunches", "highknees", "jumping", "lunges"];
+// const template = Handlebars.compile(landing, { strict: true });
+// const result = template();
+// view.innerHTML = result;
 
-start.onclick = () => {
-  setRandomChallenge();
-};
+timer.start({ countdown: true, startValues: { seconds: 10 } });
 
-let setRandomInterval = () => {
-  let randomDelay = (Math.floor(Math.random() * 15) + 60 + 1) * 1000;
+timeClock.innerHTML = timer.getTimeValues().toString();
 
-  setTimeout(() => {
-    setRandomChallenge();
-  }, randomDelay);
-};
-
-let setRandomChallenge = () => {
-  if (document.visibilityState === "hidden") alert.play();
-  let newGifUrl =
-    "./img/" + exercise[Math.floor(Math.random() * exercise.length)] + ".gif";
-  addGif.setAttribute("src", newGifUrl);
-  setRandomInterval();
-};
-
-window.addEventListener("focus", () => {
-  alert.pause();
+timer.addEventListener("secondsUpdated", function(e) {
+  timeClock.innerHTML = timer.getTimeValues().toString();
 });
+
+timer.addEventListener("targetAchieved", function(e) {
+  search.style = "display:none";
+  exercise.style = "display:grid";
+});
+// let audio = new Audio("../assets/alert.mp3");
+
+// let exercise = ["burpees", "crunches", "highknees", "jumping", "lunges"];
+
+// start.onclick = () => {
+//   setRandomChallenge();
+// };
+
+// let setRandomInterval = () => {
+//   let randomDelay = (Math.floor(Math.random() * 15) + 60 + 1) * 1000;
+
+//   setTimeout(() => {
+//     setRandomChallenge();
+//   }, randomDelay);
+// };
+
+// let setRandomChallenge = () => {
+//   if (document.visibilityState === "hidden") alert.play();
+//   let newGifUrl =
+//     "./img/" + exercise[Math.floor(Math.random() * exercise.length)] + ".gif";
+//   addGif.setAttribute("src", newGifUrl);
+//   setRandomInterval();
+// };
+
+// window.addEventListener("focus", () => {
+//   alert.pause();
+// });
---
+++ js/view.js
@@ -0,0 +1,16 @@
+//Templates
+let landing = `
+  <div class="nav">
+      <h1><span>Bro</span>grammer !!</h1>
+  </div>
+  <div class="logo">
+      <img src="assets/yoga.svg" class="tada" alt="" />
+  </div>
+  <div class="start-button">
+      <div class="custom">
+          Take Custom Challenge
+      </div>
+      <div class="create">
+          Create Own Challenge
+      </div>
+  </div>`;
