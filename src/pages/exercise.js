d45e75882ed3d128b02f51adef6f64706342f90b
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2019-08-30 00:57:24 +0530

    [update] add exercise randomizer

--- src/pages/exercise.js
+++ src/pages/exercise.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import NavBar from '../components/navbar';
-import highKneesGif from '../images/highknees.gif';
+import Exercise from '../components/exercise';
 import './index.css';
 
-const ExercisePage = () => (
-  <>
-    <NavBar />
-    <div className="exercise-area" id="exercise">
-      <div className="exercise-message">
-        <h3>Go kill it, you beast</h3>
-      </div>
-      <div className="exercise-gif">
-        <img src={highKneesGif} alt="" />
-      </div>
-      <div className="exercise-info">
-        <div className="reps">
-          <h3>
-            <span>Reps:</span> 25
-          </h3>
+// exercises gif
+import burpeesGif from '../images/burpees.gif';
+import highKneesGif from '../images/highknees.gif';
+import lungesGif from '../images/lunges.gif';
+import pushupGif from '../images/pushup.gif';
+import squatsGif from '../images/squats.gif';
+import crunchesGif from '../images/crunches.gif';
+import jumpingGif from '../images/jumping.gif';
+import mountainClimbersGif from '../images/mountainclimbers.gif';
+import planksGif from '../images/planks.gif';
+import stairSteppingGif from '../images/stairstepping.gif';
+
+const ExercisePage = () => {
+  const randomExercise = [
+    { gif: burpeesGif, reps: 3 },
+    { gif: highKneesGif, reps: 100 },
+    { gif: lungesGif, reps: 100 },
+    { gif: pushupGif, reps: 100 },
+    { gif: squatsGif, reps: 100 },
+    { gif: crunchesGif, reps: 100 },
+    { gif: jumpingGif, reps: 100 },
+    { gif: mountainClimbersGif, reps: 100 },
+    { gif: planksGif, reps: 100 },
+    { gif: stairSteppingGif, reps: 100 }
+  ][Math.floor(Math.random() * 10)];
+
+  const { gif, reps, time } = randomExercise;
+
+  return (
+    <>
+      <NavBar />
+      <div className="exercise-area" id="exercise">
+        <div className="exercise-message">
+          <h3>Go kill it, you beast</h3>
         </div>
-        <div className="time">
-          <h3>
-            <span>Duration:</span> 3 mins
-          </h3>
+        <Exercise gif={gif} reps={reps} time={time} />
+        <div className="action">
+          <div className="start-exercise">Start Exercise Now !!</div>
         </div>
       </div>
-      <div className="action">
-        <div className="start-exercise">Start Exercise Now !!</div>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default ExercisePage;
