e93b36c713d265b62665d2c5438613331abdad2a
okaberin <okaberin@stiens.gate>
2019-08-30 18:04:44 +0530

    [update] minimize resourses by fetching gifs directly from github

--- src/pages/exercise.js
+++ src/pages/exercise.js
@@ -2,34 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Timer } from 'easytimer.js';
 import NavBar from '../components/navbar';
 import Exercise from '../components/exercise';
+import exercises from '../assets/exercises';
 import './index.css';
 
 import alertAudioFile from '../assets/alert.wav';
-
-// exercises gif
-import burpeesGif from '../images/burpees.gif';
-import highKneesGif from '../images/highknees.gif';
-import lungesGif from '../images/lunges.gif';
-import pushupGif from '../images/pushup.gif';
-import squatsGif from '../images/squats.gif';
-import crunchesGif from '../images/crunches.gif';
-import jumpingGif from '../images/jumping.gif';
-import mountainClimbersGif from '../images/mountainclimbers.gif';
-import planksGif from '../images/planks.gif';
-import stairSteppingGif from '../images/stairstepping.gif';
-
-const exercises = [
-  { gif: burpeesGif, reps: 3, duration: 3 },
-  { gif: highKneesGif, reps: 100, duration: 1 },
-  { gif: lungesGif, reps: 100, duration: 2 },
-  { gif: pushupGif, reps: 100, duration: 1 },
-  { gif: squatsGif, reps: 100, duration: 2 },
-  { gif: crunchesGif, reps: 100 },
-  { gif: jumpingGif, reps: 100 },
-  { gif: mountainClimbersGif, reps: 100 },
-  { gif: planksGif, reps: 100 },
-  { gif: stairSteppingGif, reps: 100 }
-];
 
 const alarm = new Audio(alertAudioFile);
 let isActive = false;
@@ -43,7 +19,20 @@ const stopAlarm = () => {
   alarm.pause();
 };
 
+const random = () => {
+  const random = localStorage.getItem('random')
+    ? localStorage.getItem('random')
+    : localStorage.setItem(
+        'random',
+        Math.floor(Math.random() * exercises.length)
+      );
+
+  return random;
+};
+
 const ExercisePage = () => {
+  const randomNumber = random();
+
   const [time, setTime] = useState(null);
   const [start, setStart] = useState(false);
   const [timer, setTimer] = useState(null);
@@ -63,28 +52,16 @@ const ExercisePage = () => {
 
     if (randomExercise && randomExercise.reps && start) {
       localStorage.removeItem('random');
-      window.location = '/success';
+      window.location = '/';
     }
   };
 
   useEffect(() => {
     setTimer(new Timer());
-
-    const random = () => {
-      const random = localStorage.getItem('random')
-        ? localStorage.getItem('random')
-        : localStorage.setItem(
-            'random',
-            Math.floor(Math.random() * exercises.length)
-          );
-
-      return random;
-    };
-
-    setRandomExercise(exercises[random()]);
+    setRandomExercise(exercises[randomNumber]);
 
     // setRandomExercise(exercises[0]);
-  }, []);
+  }, [randomNumber]);
 
   useEffect(() => {
     if (randomExercise && randomExercise.duration && start) {
