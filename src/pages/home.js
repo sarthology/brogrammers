c39e0f1904631e9fdc099d076d3c11285193db75
akshit <ajindal126@gmail.com>
2019-08-30 17:57:11 +0530

    custom page changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,14 +3,22 @@ import { Timer } from 'easytimer.js';
 import NavBar from '../components/navbar';
 import search from '../images/search.svg';
 import './index.css';
+import queryString from 'query-string';
 
 const HomePage = () => {
   const [time, setTime] = useState(null);
 
   useEffect(() => {
+    let query = queryString.parse(document.location.search);
+    let customSeconds;
+    if(query && query.customData) {
+      let customData = JSON.parse(localStorage.getItem("customData"));
+      customSeconds = 3600 * customData["frequency"];
+    }
+  
     const timer = new Timer();
 
-    timer.start({ countdown: true, startValues: { seconds: 10 } });
+    timer.start({ countdown: true, startValues: { seconds: customSeconds ? customSeconds : 10 } });
 
     timer.addEventListener('secondsUpdated', function(e) {
       setTime(timer.getTimeValues().toString());
@@ -18,6 +26,9 @@ const HomePage = () => {
 
     timer.addEventListener('targetAchieved', function(e) {
       setTime(null);
+      if(query && query.customData) {
+        return window.location = "/exercise?customData=true";
+      }
       window.location = '/exercise';
     });
   }, []);
