f770f18827517dd9b75a21fdc56a1b2fbec24b40
okaberin <okaberin@stiens.gate>
2019-08-30 14:38:34 +0530

    [update] add per exercise timer

--- src/pages/exercise.js
+++ src/pages/exercise.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Timer } from 'easytimer.js';
 import NavBar from '../components/navbar';
 import Exercise from '../components/exercise';
 import './index.css';
@@ -15,21 +16,73 @@ import mountainClimbersGif from '../images/mountainclimbers.gif';
 import planksGif from '../images/planks.gif';
 import stairSteppingGif from '../images/stairstepping.gif';
 
-const ExercisePage = () => {
-  const randomExercise = [
-    { gif: burpeesGif, reps: 3 },
-    { gif: highKneesGif, reps: 100 },
-    { gif: lungesGif, reps: 100 },
-    { gif: pushupGif, reps: 100 },
-    { gif: squatsGif, reps: 100 },
-    { gif: crunchesGif, reps: 100 },
-    { gif: jumpingGif, reps: 100 },
-    { gif: mountainClimbersGif, reps: 100 },
-    { gif: planksGif, reps: 100 },
-    { gif: stairSteppingGif, reps: 100 }
-  ][Math.floor(Math.random() * 10)];
+const exercises = [
+  { gif: burpeesGif, reps: 3, duration: 3 },
+  { gif: highKneesGif, reps: 100, duration: 1 },
+  { gif: lungesGif, reps: 100, duration: 2 },
+  { gif: pushupGif, reps: 100, duration: 1 },
+  { gif: squatsGif, reps: 100, duration: 2 },
+  { gif: crunchesGif, reps: 100 },
+  { gif: jumpingGif, reps: 100 },
+  { gif: mountainClimbersGif, reps: 100 },
+  { gif: planksGif, reps: 100 },
+  { gif: stairSteppingGif, reps: 100 }
+];
 
-  const { gif, reps, time } = randomExercise;
+const ExercisePage = () => {
+  const [time, setTime] = useState(null);
+  const [start, setStart] = useState(false);
+  const [timer, setTimer] = useState(null);
+  const [randomExercise, setRandomExercise] = useState(null);
+
+  const handleStartExercise = e => {
+    e.preventDefault();
+
+    if (start) {
+      timer.pause();
+    }
+
+    setStart(!start);
+  };
+
+  useEffect(() => {
+    setTimer(new Timer());
+
+    setRandomExercise(
+      exercises[Math.floor(Math.random() * (exercises.length + 1))]
+    );
+
+    // setRandomExercise(exercises[0]);
+  }, []);
+
+  useEffect(() => {
+    if (randomExercise && randomExercise.duration && start) {
+      timer.start({
+        countdown: true,
+        startValues: { minutes: randomExercise.duration }
+      });
+
+      timer.addEventListener('secondsUpdated', function(e) {
+        setTime(timer.getTimeValues().toString());
+      });
+
+      timer.addEventListener('targetAchieved', function(e) {
+        setTime(null);
+        window.location = '/';
+      });
+    } else if (randomExercise && randomExercise.reps && start) {
+      timer.start();
+
+      timer.addEventListener('secondsUpdated', function(e) {
+        setTime(timer.getTimeValues().toString());
+      });
+
+      timer.addEventListener('targetAchieved', function(e) {
+        setTime(null);
+        window.location = '/';
+      });
+    }
+  }, [randomExercise, start, timer]);
 
   return (
     <>
@@ -38,9 +91,23 @@ const ExercisePage = () => {
         <div className="exercise-message">
           <h3>Go kill it, you beast</h3>
         </div>
-        <Exercise gif={gif} reps={reps} time={time} />
+        {randomExercise && (
+          <Exercise
+            gif={randomExercise.gif}
+            reps={randomExercise.reps}
+            time={
+              time
+                ? time
+                : randomExercise.duration
+                ? randomExercise.duration + ' minutes'
+                : null
+            }
+          />
+        )}
         <div className="action">
-          <div className="start-exercise">Start Exercise Now !!</div>
+          <div className="start-exercise" onClick={handleStartExercise}>
+            {start ? 'Stop Exercise Now !!' : 'Start Exercise Now !!'}
+          </div>
         </div>
       </div>
     </>
