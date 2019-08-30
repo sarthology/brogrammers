4ff51e3e3ded042bc36900aad7b56a12b821e50f
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2019-08-31 01:48:05 +0530

    [update] add api

---
+++ src/api/exercises.js
@@ -0,0 +1,348 @@
+const Exercises = {
+  'Bicycle Crunches': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/bicyclecrunches.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Burpees: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/burpees.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Butt Kicks': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/buttkicks.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Calf Raises': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/calfraises.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Crunches: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/crunches.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Flutter Kicks': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/flutterkicks.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Glute Bridges': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/glutebridges.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'High Knees': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/highknees.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Jumping: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/jumping.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Jump Lunges': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/jumplunges.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Jump Squats': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/jumpsquats.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Lunges: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/lunges.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Mountain Climber': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/mountainclimbers.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Planks: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/planks.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Pullups: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/pullups.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Pushups: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/pushup.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Side Lunges': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/sidelunges.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Situps: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/situps.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Spinal Balance': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/spinalbalance.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Squats: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/squats.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Stair Stepping': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/stairstepping.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  'Superman Raises': {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/supermanraises.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  },
+
+  Tricedips: {
+    gif:
+      'https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/tricepdips.gif',
+    reps: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    },
+    duration: {
+      easy: 1,
+      medium: 2,
+      hard: 3
+    }
+  }
+};
+
+export default Exercises;
