c3723f627a32486e249ff80cc3845c2c11903ac8
okaberin <okaberin@stiens.gate>
2019-08-31 14:01:26 +0530

    [update] add xenox link & hide popup on click

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { navigate } from 'gatsby';
 import './index.css';
 import yoga from '../assets/yoga.svg';
 import product from '../assets/product.png';
 import fire from '../assets/fire.svg';
 
-const IndexPage = () => (
-  <>
-    <div className="nav">
-      <h1>
-        <span>Bro</span>grammer!
-      </h1>
-    </div>
-    <div className="logo">
-      <img src={yoga} className="tada" alt="" />
-    </div>
-    <div className="start-button">
-      <div
-        className="custom"
-        onClick={e => {
-          e.preventDefault();
-          navigate('/home');
-        }}
-      >
-        Take Random Challenge
+const IndexPage = () => {
+  const [popup, setPopup] = useState(true);
+
+  return (
+    <>
+      <div className="nav">
+        <h1>
+          <span>Bro</span>grammer!
+        </h1>
       </div>
-      <div
-        className="create"
-        onClick={e => {
-          e.preventDefault();
-          navigate('/custom');
-        }}
-      >
-        Create Your Own Challenge
+      <div className="logo">
+        <img src={yoga} className="tada" alt="" />
       </div>
-    </div>
-    <div className="producthunt">
-      <div className="close-it">x</div>
-      <img src={product} alt="" />
-      <div className="button-pro">
-        <p>Hey, lovely people! We're on ProductHunt!</p>
-        <div className="product-link">Find us here</div>
+      <div className="start-button">
+        <div
+          className="custom"
+          onClick={e => {
+            e.preventDefault();
+            navigate('/home');
+          }}
+        >
+          Take Random Challenge
+        </div>
+        <div
+          className="create"
+          onClick={e => {
+            e.preventDefault();
+            navigate('/custom');
+          }}
+        >
+          Create Your Own Challenge
+        </div>
       </div>
-    </div>
-    <div className="copyright-link">
-      <p>
-        Made with <span>❤</span> by{' '}
-        <a href="" target="_blank" className="team-xenox-link">
-          Team XenoX
-          <img src={fire} alt="" />
+      {popup && (
+        <div className="producthunt">
+          <div
+            className="close-it"
+            onClick={e => {
+              e.preventDefault();
+              setPopup(false);
+            }}
+          >
+            x
+          </div>
+          <img src={product} alt="" />
+          <div className="button-pro">
+            <p>Hey, lovely people! We're on ProductHunt!</p>
+            <div className="product-link">Find us here</div>
+          </div>
+        </div>
+      )}
+      <div className="copyright-link">
+        <p>
+          Made with <span>❤</span> by{' '}
+          <a
+            href="https://github.com/sarthology/XenoXMutliverse/tree/master"
+            className="team-xenox-link"
+          >
+            Team XenoX
+            <img src={fire} alt="" />
+          </a>
+        </p>
+        <a href="" className="join-us">
+          Join us
         </a>
-      </p>
-      <a href="" className="join-us">
-        Join us
-      </a>
-    </div>
-  </>
-);
+      </div>
+    </>
+  );
+};
 
 export default IndexPage;
