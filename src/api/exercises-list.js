fb294da30c89ac1aa74601e862acfbe34aa3f7b4
Latika Gandhi <latikagandhi97@gmail.com>
2019-08-31 13:01:04 +0530

    conflict resolve

---
+++ src/api/exercises-list.js
@@ -0,0 +1,27 @@
+const ExercisesList = [
+  'Bicycle Crunches',
+  'Burpees',
+  'Butt Kicks',
+  'Calf Raises',
+  'Crunches',
+  'Flutter Kicks',
+  'Glute Bridges',
+  'High Knees',
+  'Jumping',
+  'Jump Lunges',
+  'Jump Squats',
+  'Lunges',
+  'Mountain Climber',
+  'Planks',
+  'Pullups',
+  'Pushups',
+  'Side Lunges',
+  'Situps',
+  'Spinal Balance',
+  'Squats',
+  'Stair Stepping',
+  'Superman Raises',
+  'Tricep Dips'
+];
+
+export default ExercisesList;
